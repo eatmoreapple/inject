80c610e9de7c46ce6a30e3059dd971229b2c48db
eatmoreapple <eatmoreorange@gmail.com>
2022-02-19 13:37:31 +0800

    auto wired with name

--- .examples/main.go
+++ .examples/main.go
@@ -18,22 +18,23 @@ func (r RealQuacker) Quack() {
 }
 
 func main() {
-	injecter := inject.New()
 
 	var bird struct {
-		Quacker Quacker `inject:"main.RealQuacker"`
+		Quacker     Quacker     `inject:""`
+		RealQuacker RealQuacker `inject:"main.RealQuacker"`
 	}
 	quacker := RealQuacker{Name: "Daffy Duck"}
 
 	// Inject the quacker into the struct
 	// if Name is empty, use pkg path instead
-	instance := inject.Instance{Value: &quacker, Name: ""}
-	if err := injecter.Register(&instance); err != nil {
+	instance := inject.Instance{Value: quacker, Name: ""}
+	if err := inject.RegisterInstance(&instance); err != nil {
 		panic(err)
 	}
-	if err := injecter.AutoWired(&bird); err != nil {
+	if err := inject.AutoWired(&bird); err != nil {
 		panic(err)
 	}
 
-	bird.Quacker.Quack() // Daffy Duck: Quack!
+	bird.Quacker.Quack()     // Daffy Duck: Quack!
+	bird.RealQuacker.Quack() // Daffy Duck: Quack!
 }
---
+++ inject.go
@@ -0,0 +1,176 @@
+package inject
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+type Injector interface {
+	Register(is ...*Instance) error
+	AutoWired(vs ...interface{}) error
+}
+
+func New() Injector {
+	return &Instance{base: true}
+}
+
+type Instance struct {
+	// Name of the instance
+	// If empty, the instance will be registered as the pkg and type name
+	Name string
+	// Value of the instance
+	Value      interface{}
+	container  map[string]*Instance
+	base       bool
+	initialize bool
+}
+
+func (i *Instance) init() error {
+	if i.initialize {
+		return nil
+	}
+	i.initialize = true
+	i.container = make(map[string]*Instance)
+	if i.base {
+		return nil
+	}
+	if i.Value == nil {
+		return errors.New("inject: instance value is nil")
+	}
+	rv := reflect.ValueOf(i.Value)
+	ri := reflect.Indirect(rv)
+	rt := ri.Type()
+	if i.Name == "" {
+		i.Name = strings.Replace(rt.PkgPath(), "/", ".", -1) + "." + rt.Name()
+	}
+	i.container[i.Name] = i
+	// is a struct
+	if ri.Kind() == reflect.Struct {
+		for j := 0; j < ri.NumField(); j++ {
+			field := ri.Field(j)
+			field = reflect.Indirect(field)
+			if field.Kind() == reflect.Struct {
+				instance := Instance{Value: field.Interface(), Name: i.Name + "." + rt.Field(j).Name}
+				if err := instance.init(); err != nil {
+					return err
+				}
+				for k, v := range instance.container {
+					i.container[k] = v
+				}
+				continue
+			}
+			i.container[i.Name+"."+rt.Field(j).Name] = &Instance{Value: field.Interface(), Name: i.Name + "." + rt.Field(j).Name}
+		}
+	}
+	return nil
+}
+
+func (i *Instance) Register(is ...*Instance) error {
+	if err := i.init(); err != nil {
+		return err
+	}
+	for _, s := range is {
+		if err := s.init(); err != nil {
+			return err
+		}
+		for k, v := range s.container {
+			i.container[k] = v
+		}
+	}
+	return nil
+}
+
+func (i *Instance) AutoWired(vs ...interface{}) error {
+	for _, v := range vs {
+		rv := reflect.ValueOf(v)
+		if rv.Kind() != reflect.Ptr {
+			return errors.New("inject: auto wired value must be a pointer")
+		}
+		rv = reflect.Indirect(rv)
+		t := reflect.TypeOf(v).Elem()
+		if rv.Kind() != reflect.Struct {
+			return errors.New("inject: auto wired value must be a struct")
+		}
+		for j := 0; j < rv.NumField(); j++ {
+			field := rv.Field(j)
+			inject := t.Field(j).Tag.Get("inject")
+			if inject == "-" {
+				continue
+			}
+			if field.CanSet() {
+				if field.Kind() == reflect.Interface {
+					// auto wired interface
+					if inject == "" {
+						var ok bool
+						for _, v := range i.container {
+							rv := reflect.ValueOf(v.Value)
+							// check if the type of the instance is assignable to the interface
+							if rv.Type().Implements(field.Type()) {
+								field.Set(rv)
+								ok = true
+								break
+							}
+						}
+						if !ok {
+							return fmt.Errorf("inject: can not find a instance for %s", field.Type().String())
+						}
+						continue
+					} else {
+						// get the instance by name
+						v, ok := i.container[inject]
+						// check if the instance is registered
+						if !ok {
+							return errors.New("inject: auto wired value not found")
+						}
+						rv := reflect.ValueOf(v.Value)
+						// check if the type of the instance is assignable to the interface
+						if rv.Type().Implements(field.Type()) {
+							field.Set(rv)
+							continue
+						} else {
+							return fmt.Errorf("inject: type of %s is not assignable to %s", rv.Type().String(), field.Type().String())
+						}
+					}
+				} else {
+					// auto wired value
+					v, ok := i.container[inject]
+					// check if the instance is registered
+					if !ok {
+						return errors.New("inject: auto wired value not found")
+					}
+					rv := reflect.ValueOf(v.Value)
+					if rv.Type().AssignableTo(field.Type()) {
+						field.Set(rv)
+						continue
+					} else {
+						return fmt.Errorf("inject: type of %s is not assignable to %s", rv.Type().String(), field.Type().String())
+					}
+				}
+			}
+		}
+
+	}
+	return nil
+}
+
+var defaultInstance = New()
+
+func RegisterInstance(is ...*Instance) error {
+	return defaultInstance.Register(is...)
+}
+
+func AutoWired(vs ...interface{}) error {
+	return defaultInstance.AutoWired(vs...)
+}
+
+func Register(vs ...interface{}) error {
+	for _, v := range vs {
+		instance := &Instance{Value: v}
+		if err := RegisterInstance(instance); err != nil {
+			return err
+		}
+	}
+	return nil
+}
